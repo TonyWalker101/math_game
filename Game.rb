49080debd617ecdcbfe3370fd048cc6cdbe5d038
Tony <awalker780@gmail.com>
2022-04-09 12:34:53 -0400

    Add default parameters for player names if none is given

--- Game.rb
+++ Game.rb
@@ -4,22 +4,21 @@ require "./Player"
 class Game
   attr_accessor :player1, :player2, :current_player, :game
   def initialize(players)
-    @player1 = Player.new(players[0])
-    @player2 = Player.new(players[1])
+    @player1 = Player.new(players[0] || "Player1")
+    @player2 = Player.new(players[1] || "Player2")
     @current_player = @player1
     @game = Turn.new
   end
 
-
   def game_on
 
-    def recursion 
+    def keep_playing 
       @game.ask_question(@current_player)
       @current_player = @current_player == @player1 ?  @player2 : @player1
     end
 
     while @player1.lives != 0 && @player2.lives != 0 do
-      recursion
+      keep_playing
       
       if @player1.lives > 0 && @player2.lives > 0
         puts "#{player1.name}: #{player1.lives}/3 #{player2.name}: #{player2.lives}/3"
